3261d164e87791739d23fcf604d2e8d7033d33da
Jeremy Stewart <jerstew@users.noreply.github.com>
2025-10-24 13:38:56 -0500

    Add initial site config; comment into from index

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://jerstew.com/", // replace this with your deployed domain
+  author: "Jeremy Stewart",
+  profile: "https://jerstew.com/",
+  desc: "Personal blog and portfolio of Jeremy Stewart",
+  title: "Jeremy Stewart",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -19,5 +19,5 @@ export const SITE = {
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "US/Central", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
